2bd2d35ed158e5b66021bc73f4e4d3fdd7ade113
anhminh109 <minnhanhle2406@gmail.com>
2026-06-03 05:02:23 +0700

    postgres render

--- src/java/com/bingo/util/DBConnection.java
+++ src/java/com/bingo/util/DBConnection.java
@@ -5,20 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=bingo_game;encrypt=true;trustServerCertificate=true";;
-    private static final String USERNAME = "sa";
-    private static final String PASSWORD = "123";
+
+    private static final String URL = System.getenv("postgresql://anhminh:NRqMMpH6scPxiw1S9aWGrXkhdXLR9fXw@dpg-d8fh9brbc2fs73eoes3g-a.singapore-postgres.render.com/bingo_game_2ocr");
 
     private DBConnection() {
     }
 
     public static Connection getConnection() throws SQLException {
         try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException ex) {
-            throw new SQLException("Không tìm thấy SQL Server JDBC Driver. Hãy thêm mssql-jdbc vào WEB-INF/lib.", ex);
+            throw new SQLException("Không tìm thấy PostgreSQL JDBC Driver.", ex);
         }
 
-        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        return DriverManager.getConnection(URL);
     }
 }
